cd581a98172fe02c2400b83e8942142ebce2753e
goutamprajapat554@gmail.com <goutamprajapat554@gmail.com>
2022-07-31 17:32:25 +0530

    Coming soon page

---
+++ main.js
@@ -0,0 +1,35 @@
+const seconds = document.querySelector(".seconds .number"),
+  hours = document.querySelector(".hours .number"),
+  minutes = document.querySelector(".minutes .number"),
+  days = document.querySelector(".days .number");
+
+
+
+let secValue = 60,
+  minValue = 60,
+  hourValue = 02,
+  dayValue = 09;
+
+const timefunction = setInterval(() => {
+  secValue--;
+  if (secValue === 0) {
+    minValue--;
+    secValue = 60;
+  }
+  if (minValue === 0) {
+    hourValue--;
+    minValue = 60;
+  }
+  if (hourValue === 0) {
+    dayValue--;
+    hourValue = 24;
+  }
+  if (dayValue === 0) {
+    clearInterval(timefunction);
+  }
+
+  seconds.textContent = secValue < 10 ? `0${secValue}` : secValue;
+  minutes.textContent = minValue < 10 ? `0${minValue}` : minValue;
+  hours.textContent = hourValue < 10 ? `0${hourValue}` : hourValue;
+  days.textContent = dayValue < 10 ? `0${dayValue}` : dayValue;
+}, 1000);
